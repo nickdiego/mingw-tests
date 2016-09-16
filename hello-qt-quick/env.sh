f87f7581bd487c08d5821dc5666113028bc4ca4e
Nick Diego Yamane <nick.diego@gmail.com>
2016-09-15 21:15:48 -0400

    move source files to hello-qt-quick dir

--- hello-qt-quick/env.sh
+++ hello-qt-quick/env.sh
@@ -12,28 +12,35 @@ proj_set_target() {
   case $target in
     Msys*64)
       prefix='x86_64-w64-mingw32'
+      cmake=${prefix}-cmake
+      run=${prefix}-wine
       ;;
     *Linux*)
       # TODO always assuming host=target here
-      prefix=''
+      cmake=cmake
+      run=''
       ;;
     *) return 1
   esac
   target_plat=$target
-
-  # configure tools names
-  if [ ! -z $prefix ]; then
-    cmake=${prefix}-cmake
-    run=$prefix-wine
-  fi
   unset target
+  echo "####### Project dir: $projdir"
+  echo "####### Build dir: $builddir"
+  echo "####### Host platform: $host_plat"
+  echo "####### Target platform: $target_plat"
+  echo "####### Config script: $cmake"
 }
 
 pushdir() { pushd $1 > /dev/null; }
 popdir() { popd $1 > /dev/null; }
 
+push_buildir() {
+  test -d $builddir || mkdir -pv $builddir
+  pushdir $builddir || exit 1
+}
+
 proj_config() {
-  pushdir $builddir
+  push_buildir
   $cmake $projdir
   # FIXME temp (report mingw-x86-64-qt5-base-dynamic
   # bug for this)
@@ -42,34 +49,27 @@ proj_config() {
 }
 
 proj_build() {
-  pushdir $builddir
+  push_buildir
   make -j4
   popdir
 }
 
 proj_run() {
-  pushdir $builddir
+  push_buildir
   $run $exe $@
   popdir
-}
-
-host_plat="$(uname)-$(uname -m)"
-proj_set_target $1 || {
-  echo "Unrecognized Platform: '$1'"
-  return 1
 }
 
 script=$BASH_SOURCE
 projdir_rel=$(dirname "$script")
 projdir=$(pwd -L $projdir_rel)
-builddir="$projdir/build-mingw"
+builddir="$projdir/build-${target_plat}"
 buildfile="${builddir}/CMakeFiles/hellomingw.dir/build.make"
 exe=$builddir/hellomingw
+host_plat="$(uname)-$(uname -m)"
 
-test -d $builddir || mkdir -p $builddir
-
-echo "####### Project dir: $projdir"
-echo "####### Build dir: $builddir"
-echo "####### Host platform: $host_plat"
-echo "####### Target platform: $target_plat"
+proj_set_target $1 || {
+  echo "Unrecognized Platform: '$1'"
+  return 1
+}
 
